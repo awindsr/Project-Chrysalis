68bceb0f1c624caaed74da7cc23f8a33b07eda80
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-09 20:53:01 +0530

    update: styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './components/auth/Login'
-import { Provider } from 'react-redux'
-import store from './redux/store'; 
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/auth/Login";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-
   return (
-    <Provider store={store}> 
-      <Login/>
+    <Provider store={store}>
+      <div className="bg-white">
+        <Login />
+      </div>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -12,8 +12,43 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div className='bg-[#09090b] w-screen h-auto'>
-
+        <div className='bg-white w-screen h-auto flex flex-col items-center justify-center p-8'>
+            <h2 className='text-white text-2xl mb-4'>Welcome Back</h2>
+            <form onSubmit={handleSubmit} className='w-full max-w-sm'>
+                <div className='mb-4'>
+                    <label htmlFor="email" className='text-white'>Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className='w-full p-2 rounded bg-gray-800 text-white border border-gray-600'
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label htmlFor="password" className='text-white'>Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className='w-full p-2 rounded bg-gray-800 text-white border border-gray-600'
+                    />
+                </div>
+                <button type="submit" className='w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700'>
+                    Login
+                </button>
+            </form>
+            <a href="/forgot-password" className='text-white mt-2'>Forgot Password?</a>
+            <div className='my-4 text-white'>or</div>
+            <button className='w-full bg-red-600 text-white p-2 rounded hover:bg-red-700'>
+                Continue using Google
+            </button>
+            <p className='text-white mt-4'>
+                Don't have an account? <a href="/register" className='text-blue-400'>Register</a>
+            </p>
         </div>
     );
 };
